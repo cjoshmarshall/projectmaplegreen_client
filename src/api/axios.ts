bb83cfa696da0d901bccbe09a7db976f2437693a
josh <cjoshmarshall@gmail.com>
2022-11-16 22:30:06 +0530

    Axios Update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = 'http://localhost:3006/api'
+// export const BASE_URL = 'http://localhost:3006/api'
+export const BASE_URL = 'https://projectmaplegreen.herokuapp.com/api'
 
 export default axios.create({
     baseURL: BASE_URL
