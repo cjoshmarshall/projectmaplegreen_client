41124b5363a0250ab0a695ff94ae3292701a52f5
josh <cjoshmarshall@gmail.com>
2022-11-18 15:47:21 +0530

    Button Hover Fix

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -15,11 +15,10 @@ function Button({
     disabled
 }: ButtonProps) {
 
-    const [isClicked, setIsClicked]: any = React.useState(false)
 
     return (
         <button
-            className={`${className} flex place-items-center p-2 text-white rounded-lg shadow-lg drop-shadow-lg ${isClicked ? 'translate-y-1' : ''} ${disabled ? 'bg-gray-500 cursor-not-allowed' : 'bg-green hover:bg-lightgreen'} transition-all`}
+            className={`${className} flex place-items-center p-2 text-white rounded-lg shadow-lg drop-shadow-lg ${disabled ? 'bg-gray-500 cursor-not-allowed' : 'bg-green hover:bg-lightgreen'} transition-all`}
             onClick={onClick}
             disabled={disabled}>
             <span className='px-1'>{children}</span>
